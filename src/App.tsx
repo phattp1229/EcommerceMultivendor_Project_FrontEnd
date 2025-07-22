82e87388020be8035212a79e0287df7d70da8f84
PhatStar <phamtanphat469@gmail.com>
2025-07-23 00:57:58 +0700

    add homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from '@mui/material'
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import Navbar from './customer/components/Navbar/Navbar'
 
 function App() {
   return (
-    <div className="p-20">
-
-      <h1 className='font-bold text-5xl'>Shopping Mall</h1>
-
-      <Button variant='contained'>Shop E-commerce</Button>  {/* Nút có nền xanh đậm (default primary)*/}
-
-      <AddShoppingCartIcon></AddShoppingCartIcon>
+      <div className="">
+        <Navbar />
     </div>
   )
 }
---
+++ src/customer/components/Navbar/Navbar.tsx
@@ -0,0 +1,62 @@
+    import {Avatar,Badge,Box,Button,Drawer,IconButton,useMediaQuery,useTheme,} from "@mui/material";
+    import React, { useEffect, useState } from "react";
+    import "./Navbar.css";
+    import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+    import StorefrontIcon from "@mui/icons-material/Storefront";
+    import SearchIcon from "@mui/icons-material/Search";
+    import MenuIcon from "@mui/icons-material/Menu";
+    import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+    import { FavoriteBorder } from "@mui/icons-material";
+
+    const Navbar = () => {
+        const theme = useTheme();
+        const isLarge = useMediaQuery(theme.breakpoints.up("lg"));
+        const user = true; // Giả định có đăng nhập
+        return (
+            <div>
+                <Box>
+                    <div className="flex items-center justify-between px-5 lg:px-20 h-[70px] border-b">
+                        <div className="flex items-center gap-9">
+                            <div className="flex items-center gap-2">
+                                <IconButton>
+                                    <MenuIcon/>
+                                </IconButton>
+                                <h1 className="logo cursor-pointer text-lg md:text-2xl  text-[#00927c]">
+                                    Zonix Mall
+                                </h1>
+                            </div>
+                        </div>
+                        <div>   
+                            <IconButton>
+                                <SearchIcon className="text-gray-700" sx={{ fontSize: 29 }}/>
+                            </IconButton>
+                            {user ? (
+                                <Button className="flex items-center gap-2">
+                                <Avatar
+                                    sx={{ width: 29, height: 29 }}
+                                    src="https://cdn.pixabay.com/photo/2015/04/15/09/28/head-723540_640.jpg"
+                                />
+                                <h1 className="font-semibold hidden lg:block">Phat</h1>
+                                </Button>
+                            ) : (
+                                <Button variant="contained">Login</Button>
+                            )}
+
+                            <IconButton>
+                                <FavoriteBorder sx={{ fontSize: 29 }}className="text-gray-700" />
+                            </IconButton>
+                            <IconButton>
+                                <Badge badgeContent={4} color="primary">
+                                    <AddShoppingCartIcon className="text-gray-700" sx={{ fontSize: 29 }}/>
+                                </Badge>
+                            </IconButton>
+                            {isLarge && <Button startIcon={<StorefrontIcon />} variant='outlined'>
+                                Become Seller
+                            </Button>}
+                        </div>
+                    </div>
+                </Box>
+            </div>
+        )
+    }
+    export default Navbar;
