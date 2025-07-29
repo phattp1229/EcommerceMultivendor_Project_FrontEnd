9e0d3c356d292e5f9a529d93b57d4f1b10c98c96
PhatStar <phamtanphat469@gmail.com>
2025-07-29 21:16:50 +0700

    Re-add Redux Toolkit folder

---
+++ src/Redux Toolkit/Store.ts
@@ -0,0 +1,27 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { useDispatch, useSelector, type TypedUseSelectorHook } from "react-redux";
+import sellerSlice from "./Seller/sellerSlice";
+import sellerAuthenticationSlice from "./Seller/sellerAuthenticationSlice";
+import AuthSlice from "./Customer/AuthSlice";
+import CustomerSlice from "./Customer/Customer/CustomerSlice";
+const rootReducer = combineReducers({
+    // customer
+    auth: AuthSlice,
+    homePage: CustomerSlice,
+    //seller
+    sellers: sellerSlice,
+    sellerAuth: sellerAuthenticationSlice
+});
+
+const store = configureStore({
+    reducer: rootReducer,
+    // No need to define middleware unless you're adding custom ones
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof rootReducer>;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default store;
