8545f6d47177eb89754feec5936a4fd2b3b8e84b
PhatStar <phamtanphat469@gmail.com>
2025-08-08 01:53:05 +0700

    update stripe payment

--- src/routes/CustomerRoutes.tsx
+++ src/routes/CustomerRoutes.tsx
@@ -49,7 +49,7 @@ const CustomerRoutes = () => {
         <Route path='/checkout/address' element={<Address />} />
         <Route path='/account/*' element={<Profile />} />
         <Route path='/login' element={<Auth/>} />
-        <Route path="/payment-success/:paymentOrderId" element={<PaymentSucess/>} /> //Dành cho Stripe
+        <Route path="/payment-success" element={<PaymentSucess/>} /> //Dành cho Stripe
         <Route path='*' element={<NotFound/>} />
       </Routes>
       <Footer />
