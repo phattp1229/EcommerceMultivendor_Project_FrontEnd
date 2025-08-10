2bf35634ad6f52d5db921f9634989c89113a931c
PhatStar <phamtanphat469@gmail.com>
2025-08-10 19:45:22 +0700

    update koc

--- src/admin/pages/Koc/ListKocTable.tsx
+++ src/admin/pages/Koc/ListKocTable.tsx
@@ -83,8 +83,8 @@ export default function KocListTable() {
 
   const columns: GridColDef[] = [
     { field: "id", headerName: "ID", width: 80, headerAlign: "center", align: "center", headerClassName: "col-id" },
-    { field: "name", headerName: "Customer", flex: 1, minWidth: 220, headerClassName: "col-id" },
-    { field: "customerId", headerName: "Customer ID", width: 140, headerAlign: "center", align: "center" , headerClassName: "col-id"},
+    { field: "name", headerName: "Name", flex: 1, minWidth: 220, headerClassName: "col-id" },
+    { field: "kocId", headerName: "Koc ID", width: 140, headerAlign: "center", align: "center" , headerClassName: "col-id"},
     { field: "email", headerName: "Email", flex: 1, minWidth: 260 , headerClassName: "col-id"},
     {
       field: "accountStatus",
